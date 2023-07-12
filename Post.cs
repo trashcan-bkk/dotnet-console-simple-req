24e8bfe92371732a6bfc15d3c639e1c73772def1
panitarn <panitarn.work@gmail.com>
2023-07-12 14:22:12 +0700

    Initial commit

---
+++ Post.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_console_simple_req
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace dotnet_console_simple_req
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
+                var newPost = new Post() 
+                {
+                    Title = "Testxx",
+                    Body = "Hello worldxx",
+                    UserId = 12 
+                };
+                var newPostJson = JsonConvert.SerializeObject(newPost);
+                var payload = new StringContent(newPostJson, Encoding.UTF8, "application/json");
+                var result = client.PostAsync(endpoint, payload).Result.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
